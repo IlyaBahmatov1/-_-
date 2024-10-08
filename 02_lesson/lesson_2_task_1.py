80e84626a5a6593539612cdae569b48d07be8dab
unknown <[email protected]>
2024-10-08 19:34:27 +0300

    DZ2

---
+++ 02_lesson/lesson_2_task_1.py
@@ -0,0 +1,10 @@
+lst = ["Виноград", "Персик", "Груша", "Апельсин", "Банан", "Яблоко"]
+
+length = len(lst)
+print(length)
+
+whatif = lst[0]
+print(whatif)
+
+whatif = lst[5]
+print(whatif)
---
+++ 02_lesson/lesson_2_task_3.py
@@ -0,0 +1,7 @@
+def square(a, b):
+    return square(a*b)
+
+a = float(input('Введите первое слагаемое: '))
+b = float(input('Введите второе слагаемое: '))
+
+print(a*b)
